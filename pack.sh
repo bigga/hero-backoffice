b7e37d4c26194585c7cd558208c6a0766e5c2a2e
Chanan Gosonsaksakun <craxidile@gmail.com>
2017-10-27 14:33:49 +0700

    new version

--- pack.sh
+++ pack.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+mkdir -p ../hero-backoffice-prod/server
 cp -rf ./build ../hero-backoffice-prod/build
-cp -rf ./server ../hero-backoffice-prod/server
+cp -rf ./server/config ../hero-backoffice-prod/server/config
+cp -rf ./server/controllers ../hero-backoffice-prod/server/controllers
+cp -rf ./server/middleware ../hero-backoffice-prod/server/middleware
+cp -rf ./server/utils ../hero-backoffice-prod/server/utils
+cp -rf ./server/.babelrc ../hero-backoffice-prod/server/.babelrc
+cp -rf ./server/app.js ../hero-backoffice-prod/server/app.js
+cp -rf ./server/package.json ../hero-backoffice-prod/server/package.json
 cd ../hero-back-office-prod
 git add *
 git commit -a -m 'new version'
